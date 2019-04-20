e13707bb896a2ecd594eaf85057453fc57980c76
Shogher Ghazaryan <shogher.ghazaryan@istc.com>
2019-04-20 14:15:46 +0400

    add contact

--- crm_system/src/TableComponents/menu/menu.js
+++ crm_system/src/TableComponents/menu/menu.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import './menu.css';
+import Button from '../Button/Button';
 
 class Menu extends Component{
   render(){
     return(
           <div className="menu">
                   <ul className="list_menu">
-                      <li className="menu_item">Contacts </li>
-                       <li className="menu_item">Mailing list</li>
+                      <li className="menu_item">
+                      <Button name = {"Contats"}  className= "CB1 menu_btn" />
+                      
+                       </li>
+                       <li className="menu_item">
+                       <Button name = {"Mailing List"}  className= "CB1 menu_btn" />
+                       </li>
                  </ul>
           </div>
     );
--- crm_system/src/TableComponents/tableContent/tableContent.js
+++ crm_system/src/TableComponents/tableContent/tableContent.js
@@ -1,12 +1,30 @@
 import React, { Component, Fragment } from 'react';
 import './tableContent.css';
-import TableFooter from '../tableFooter/tableFooter';
+//import TableFooter from '../tableFooter/tableFooter';
 
 class TableContent extends Component{
+    state = {
+        editTd:false,
+        data: []
+        };
+
+   componentDidMount(){  
+    fetch('http://visual.istclabz.com:2112/api/contacts')
+        .then((resp) => {return resp.json()})
+        .then((results) => { 
+         this.setState({data: results})
+    })
+
+  }  
+  
+  editTr = () => {this.setState({editTd:true})
+  console.log(this.state.editTd)
+  }
+
     render() {
         return (
             <Fragment>
-               
+              <div className="ddd"> 
             <thead>
                 <tr>
                     <th>Select</th>
@@ -17,9 +35,25 @@ class TableContent extends Component{
                     <th>Email</th>
                     <th>Edit</th>
                 </tr>
-            
             </thead>
-            <TableFooter/>            
+            
+            {this.state.data.map((v,i) =>
+          
+          <tr>
+            <td><input type = "checkbox"/></td>
+            <td style={{contenteditable:this.state.editTd}}>vgdgh</td>
+            <td style={{contenteditable:true}}>464679978975</td>
+            <td style={{contenteditable:this.state.editTd}}>{"Full Name"}</td>
+            <td style={{contenteditable:this.state.editTd}}>{"Company Name"}</td>
+            <td style={{contenteditable:this.state.editTd}}>{v.Position}</td>
+            <td style={{contenteditable:this.state.editTd}}>{v.Counrty}</td>
+            <td style={{contenteditable:this.state.editTd}}>{v.Email}</td>
+            <td onClick = {this.editTr}><i className = "fa fa-pencil-square-o"></i></td>       
+          </tr>
+      
+        )
+      }           
+            </div>        
                 
             </Fragment>
         );

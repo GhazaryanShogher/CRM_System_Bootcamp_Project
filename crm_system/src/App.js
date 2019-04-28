d4acc54088934b5c52bbdd9b2da374e71a14bb4b
Mihran.Guyumjyan <mihran.guyumjyan@gmail.com>
2019-04-29 01:30:43 +0400

    Worked on Route for CRM

--- crm_system/src/App.js
+++ crm_system/src/App.js
@@ -2,14 +2,17 @@ import React, { Component } from 'react';
 import MainTable  from './TableComponents/MainTable';
 import MainPage from "./MainViewComponents/Main/Main";
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          <MainPage/>
-          {/* <MainTable/> */}
-      </div>
+      <Router>
+        <div className="App">
+          <Route path="/" component={ MainPage } exact />
+          <Route path="/Contacts" component={ MainTable } />
+        </div>
+      </Router>
     );
   }
 }

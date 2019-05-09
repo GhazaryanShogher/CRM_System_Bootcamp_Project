35a4da537e378603868e44f5e9734485cc0a9f7a
Mihran.Guyumjyan <mihran.guyumjyan@gmail.com>
2019-05-09 19:02:32 +0400

    Worked on render of Mailing Lists contacts

--- crm_system/src/TableComponents/MailingList/MailingList.js
+++ crm_system/src/TableComponents/MailingList/MailingList.js
@@ -3,7 +3,6 @@ import './MailingList.css';
 import '../tableContent/tableContent.css';
 import Div from '../../Div/Div';
 import Icon from '../Icon/Icon';
-import Input from '../Input/Input';
 import Button from '../Button/Button';
 
 class MailingList extends Component{
@@ -11,7 +10,7 @@ class MailingList extends Component{
     mailLists:[],
     status:"none",
     contactsList:"none",
-    listOfContacts:[]
+    listOfContacts:"",
   }
 
   showList = (e) => {    
@@ -64,7 +63,7 @@ class MailingList extends Component{
         <div className="mailing_list">
             <div className= "mailList_section">
                 {this.state.mailLists.map((v,i) => 
-            <div >
+            <div key={i}>
                 <Div className = {"mailList_name"}  name = {v.EmailListName} listId = {v.EmailListID} click = {this.showList}></Div>
                 <Div className = {"mailing_list_del"} click = {this.deleteRow}  name = {<Icon  className={"fa fa-trash" } id = {v.EmailListID}></Icon>} ></Div>    
                 <Div className = {"mailing_list_arr}"} name = {<Icon  className={"fa fa-chevron-right" }></Icon>} id = {v.EmailListID}></Div>
@@ -86,33 +85,29 @@ class MailingList extends Component{
             
               <div >
                 <div className="table_header">
-                    <div className="header_btn1">Select</div>
                     <div className="header_name">Full Name</div>
                     <div className="header_name">Company Name</div>
                     <div className="header_name" >Position</div>
                     <div className="header_name">Counrty</div>
                     <div className="header_name">Email</div>
-                    <div className="header_btn1">Edit</div>
                     <div className="header_btn1">Delete</div>
                 </div>
               </div>
               </div>
             <div className="overflow_div">
-            {this.state.listOfContacts.Contacts.map((v,i) => {
+            {this.state.listOfContacts !== "" ? this.state.listOfContacts.Contacts.map((v,i) => {
             
-          <div className="tbl_content">
-            <div  className="checkbox"><Input class="check" val={v.GuID} type = "checkbox"/></div>
+          return <div className="tbl_content" key={i}>
             <div className="td_style" style={{contenteditable:this.state.editTd}}>{v["Full Name"]}</div>
             <div className="td_style" style={{contenteditable:this.state.editTd}}>{v["Company Name"]}</div>
             <div className="td_style" style={{contenteditable:this.state.editTd}}>{v.Position}</div>
             <div  className="td_style" style={{contenteditable:this.state.editTd}}>{v.Country}</div>
             <div className="td_style" style={{contenteditable:this.state.editTd}}>{v.Email}</div>
-            <div  className="editbox" id = {v.GuID}><Icon  className="fa fa-cogs" aria-hidden="true" id = {v.GuID} ></Icon></div>
-            <div  className="editbox" id = {v.GuID}><Icon  className="fa fa-trash" aria-hidden="true" id = {v.GuID} ></Icon></div>    
+            <div  className="del_icon" id = {v.GuID}><Icon  className="fa fa-trash" aria-hidden="true" id = {v.GuID} ></Icon></div>    
           </div>
             }
       
-        )}   
+        ): null }   
       </div>   
             </div>
         </div>

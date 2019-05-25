ac3fba7747f62661e3944ff5926cd927661e69df
Mihran.Guyumjyan <mihran.guyumjyan@gmail.com>
2019-05-26 02:01:06 +0400

    changed Language buttons

--- crm_system/src/TableComponents/menu/menu.js
+++ crm_system/src/TableComponents/menu/menu.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import './menu.css';
-import arm from './arm.png';
-import usa from './usa.png';
 import Button from '../Button/Button';
 import { Link } from 'react-router-dom';
 import { FormattedMessage } from "react-intl";
 import * as locale from "../../Actions/locale";
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
+import Div from '../../Div/Div';
 
 class Menu extends Component{
   state = {
@@ -29,9 +28,9 @@ class Menu extends Component{
     return(
       <div className="menu">
         <Button disabled={this.state.contactButtonStatus} name = {<Link className={this.state.contactLinkStatus} to="/Contacts"><FormattedMessage id="contacts"/></Link>}  className= {"CB1 menu_btn" }/>          
-        <Button disabled={this.state.mailButtonStatus} name = {<Link className={this.state.mailLinkStatus} to="Mailinglist"><FormattedMessage id="mailinglist"/></Link>}  className= {"CB1 menu_btn"} />        
-      <img src={arm} alt="am" width="30px" height="20px" onClick = {()=>this.props.setLocale('am')}/>
-      <img src={usa} alt="en" width="30px" height="20px" onClick = {()=>this.props.setLocale('en')}/>
+        <Button disabled={this.state.mailButtonStatus} name = {<Link className={this.state.mailLinkStatus} to="Mailinglist"><FormattedMessage id="mailinglist"/></Link>}  className= {"CB1 menu_btn"} />   
+        <Div className = "language" click = {()=>this.props.setLocale('am')} name = "Am"/>
+        <Div className = "language" click = {()=>this.props.setLocale('en')} name = "Eng"/>
       </div>
     );
   }

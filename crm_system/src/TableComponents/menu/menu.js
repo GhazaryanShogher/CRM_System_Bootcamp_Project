544a4991d057e01061b6d3d80fab2ad40e304c4b
GhazaryanShogher <shogherghazaryan4@gmail.com>
2019-05-27 20:34:09 +0400

    logo icon

--- crm_system/src/TableComponents/menu/menu.js
+++ crm_system/src/TableComponents/menu/menu.js
@@ -27,18 +27,14 @@ class Menu extends Component{
   render(){
     return(
       <div className="menu">
-        <div className="lang_div">
-          <Div className = "language" click = {()=>this.props.setLocale('am')} name = "Am"/>
-          <Div className = "language" click = {()=>this.props.setLocale('en')} name = "Eng"/>
-        </div>
-        <div>
-        <Button disabled={this.state.contactButtonStatus} name = {<Link className={this.state.contactLinkStatus} to="/Contacts"><FormattedMessage id="contacts"/></Link>}  className= {"CB1 menu_btn" }/>          
-        <Button disabled={this.state.mailButtonStatus} name = {<Link className={this.state.mailLinkStatus} to="Mailinglist"><FormattedMessage id="mailinglist"/></Link>}  className= {"CB1 menu_btn"} />   
-
-        </div>
-        
-        {/* <Div className = "language" click = {()=>this.props.setLocale('am')} name = "Am"/>
-        <Div className = "language" click = {()=>this.props.setLocale('en')} name = "Eng"/> */}
+          <div className="lang_div">
+                <Div className = "language" click = {()=>this.props.setLocale('am')} name = "Arm"/>
+                <Div className = "language" click = {()=>this.props.setLocale('en')} name = "Eng"/>
+          </div>
+          <div>
+              <Button disabled={this.state.contactButtonStatus} name = {<Link className={this.state.contactLinkStatus} to="/Contacts"><FormattedMessage id="contacts"/></Link>}  className= {"CB1 menu_btn" }/>          
+              <Button disabled={this.state.mailButtonStatus} name = {<Link className={this.state.mailLinkStatus} to="Mailinglist"><FormattedMessage id="mailinglist"/></Link>}  className= {"CB1 menu_btn"} />   
+          </div>
       </div>
     );
   }
